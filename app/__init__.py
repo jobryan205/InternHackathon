0a19a47dc5849f224167585c2772b6bc34edbbbb
jobryan205 <jobryan205@gmail.com>
2016-07-15 20:59:46 -0700

    fixed configuration issue

--- app/__init__.py
+++ app/__init__.py
@@ -5,5 +5,5 @@ from flask import *
 app = Flask(__name__, template_folder='templates')
 
 # setup index blueprint
-from app.templates.index.views import index
+from app.views.index.views import index
 app.register_blueprint(index, url_prefix='/')
